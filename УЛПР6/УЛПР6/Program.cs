8d933c4cdd392a893ecad82d6de31957848a0e84
MaidLine <105170727+MaidLine@users.noreply.github.com>
2022-06-03 08:16:21 +0700

    Add files via upload

---
+++ УЛПР6/УЛПР6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace УЛПР6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i < 1000; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    int sum = 0;
+                    int i1 = i;
+                    while (i1 != 0)
+                    {
+                        sum += i1 % 10;
+                        i1 /= 10;
+                    }
+                    if (sum % 7 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
