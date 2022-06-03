7e4789800ba5ef965982b9bb3b94fab6dc01e044
MaidLine <105170727+MaidLine@users.noreply.github.com>
2022-06-03 08:19:05 +0700

    Add files via upload

---
+++ УЛПР БВ4/УЛПР БВ4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ULPR_BV4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n: ");
+            int n = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            int neededNumber = rnd.Next(1, n);
+            int k = rnd.Next(10, n);
+            bool isWin = false;
+            while (k > 0 & isWin == false)
+            {
+                Console.WriteLine($"У вас {k} попыток");
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (inputNumber == neededNumber)
+                {
+                    Console.WriteLine("Вы угадали");
+                    isWin = true;
+                }
+                else
+                {
+                    if (inputNumber > neededNumber)
+                    {
+                        Console.WriteLine("Число меньше");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число больше");
+                    }
+                    k -= 1;
+                }
+            }
+            if (k <= 0)
+            {
+                Console.WriteLine("Попытки закончились");
+            }
+        }
+    }
+}
---
+++ УЛПР БВ5/УЛПР БВ5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ULPR_BV5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] mas = new int[n];
+            //int[] mas = {1, 2, 3, 4, 5 };
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            //
+            int[] mas1 = new int[n];
+            Random rnd = new Random();
+            List <int> usedI = new List <int>();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                int replaceableI;
+                bool reRun = false;
+                do
+                {
+                    reRun = false;
+                    replaceableI = rnd.Next(0, mas.Length);
+                    for (int j = 0; j < usedI.Count; j++)
+                    {
+                        if (replaceableI == usedI[j])
+                        {
+                            reRun = true;
+                        }
+                    }
+                } 
+                while (replaceableI == i | reRun == true);
+                mas1[replaceableI] = mas[i];
+                usedI.Add(replaceableI);
+            }
+            for (int i = 0; i < mas1.Length; i++)
+            {
+                Console.WriteLine(mas1[i]);
+            }
+        }
+    }
+}
